bcf69cca32c6caaeb90ec2ff4180fc69bfba7d33
baothi <baothi246@gmail.com>
2022-10-20 19:48:13 +0700

    basically finished the cart part

--- README.md
+++ README.md
@@ -68,3 +68,7 @@ This section has moved here: [https://facebook.github.io/create-react-app/docs/d
 ### `npm run build` fails to minify
 
 This section has moved here: [https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify](https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify)
+
+npm install --save-exact react-redux@7.2.8
+
+npm install --save-exact redux@4.1.2
--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+// components
+import Navbar from "./components/Navbar";
+import CartContainer from "./components/CartContainer";
+// items
+import cartItems from "./cart-items";
+// redux stuff
+
+// store - stores data, think of state
+// reducer - function that use to update store
+
+import {createStore} from "redux"
+import reducer from "./reducer";
+// dispatch method - send action to the store
+// actions (objects) - MUST HAVE TYPE PROPERTY - What kind of action
+// don't mutate the state - redux build on IMMUTABILITY(copy)
+import { Provider } from "react-redux";
+// instial store
+const initialStore = {
+  cart:cartItems,
+  total:105,
+  amount:5
+}
+
+// store
+const store = createStore(reducer, initialStore);
 
 function App() {
+  // cart setup
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider store={store}>
+      <Navbar />
+      <CartContainer />
+    </Provider>
   );
 }
 
